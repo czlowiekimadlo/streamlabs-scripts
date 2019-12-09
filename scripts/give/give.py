75547effa8763b29ee36460e625f9e54a3d8ad90
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2019-12-09 17:14:42 +0100

    Trimming the example code

--- scripts/give/give.py
+++ scripts/give/give.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # pylint: disable=invalid-name
-"""Raffle game with lot of variation and customization to fit most streamers"""
+"""Command for transferring currency to other viewers, for a fee"""
 #---------------------------------------
 # Libraries and references
 #---------------------------------------
 import codecs
 import json
 import os
-import winsound
-import ctypes
-import random
-import time
 #---------------------------------------
 # [Required] Script information
 #---------------------------------------
-ScriptName = "Raffle"
-Website = "https://www.twitch.tv/generalrommel"
-Creator = "GeneralRommel"
+ScriptName = "Give"
+Website = "https://github.com/czlowiekimadlo/streamlabs-scripts"
+Creator = "CzlowiekImadlo"
 Version = "1.0"
-Description = "Raffle minigame"
+Description = "Currency transfer"
 #---------------------------------------
 # Variables
 #---------------------------------------
 settingsfile = os.path.join(os.path.dirname(__file__), "settings.json")
-MessageBox = ctypes.windll.user32.MessageBoxW
-MB_YES = 6
 #---------------------------------------
 # Classes
 #---------------------------------------
@@ -40,17 +34,9 @@ class Settings:
 
         else: #set variables if no custom settings file is found
             self.OnlyLive = True
-            self.Command = "!raffle"
-            self.JoinCommand = "!join"
-            self.Permission = "Caster"
-            self.PermissionInfo = ""
-            self.Usage = "Stream Chat"
-            self.WinResponse = "{0} won {1} {3} and now has {2} {3} "
-            self.StartResponse = "A raffle for {0} {1} has started! Type !join to join."
-            self.NoJoinResponse = "Nobody joined the raffle so nobody wins, try again another day."
-            self.JoinMessage = "$user has joined the raffle."
-            self.PermissionResp = "$user -> only $permission ($permissioninfo) and higher can use this command"
-            self.RaffleTime = 30.0
+            self.Command = "!give"
+            self.SuccessResponse = "{0} won {1} {3} and now has {2} {3} "
+            self.FailResponse = "A raffle for {0} {1} has started! Type !join to join."
 
     # Reload settings on save through UI
     def Reload(self, data):
@@ -70,35 +56,18 @@ class Settings:
 #---------------------------------------
 # Settings functions
 #---------------------------------------
-def SetDefaults():
-    """Set default settings function"""
-    winsound.MessageBeep()
-    returnValue = MessageBox(0, u"You are about to reset the settings, "
-                                "are you sure you want to contine?"
-                             , u"Reset settings file?", 4)
-    if returnValue == MB_YES:
-        returnValue = MessageBox(0, u"Settings successfully restored to default values"
-                                 , u"Reset complete!", 0)
-        global MySet
-        Settings.Save(MySet, settingsfile)
-
 def ReloadSettings(jsonData):
     """Reload settings on pressing the save button"""
-    global MySet
-    MySet.Reload(jsonData)
+    global ScriptSettings
+    ScriptSettings.Reload(jsonData)
 
 def SaveSettings():
     """Save settings on pressing the save button"""
-    Settings.Save(MySet, settingsfile)
+    Settings.Save(ScriptSettings, settingsfile)
 
 #---------------------------------------
 # Optional functions
 #---------------------------------------
-def OpenReadMe():
-    """Open the readme.txt in the scripts folder"""
-    location = os.path.join(os.path.dirname(__file__), "README.txt")
-    os.startfile(location)
-
 def SendResp(data, Usage, Message):
     """Sends message to Stream or discord chat depending on settings"""
     Message = Message.replace("$user", data.UserName)
@@ -128,43 +97,11 @@ def SendResp(data, Usage, Message):
 #---------------------------------------
 def Init():
     """data on Load, required function"""
-    global MySet
-    MySet = Settings(settingsfile)
-
-    if MySet.Usage == "Twitch Chat":
-        MySet.Usage = "Stream Chat"
-        Settings.Save(MySet, settingsfile)
-
-    elif MySet.Usage == "Twitch Whisper":
-        MySet.Usage = "Stream Whisper"
-        Settings.Save(MySet, settingsfile)
-
-    elif MySet.Usage == "Twitch Both":
-        MySet.Usage = "Stream Both"
-        Settings.Save(MySet, settingsfile)
-
-    global State
-    State = 0
-
-    global JoinedPlayers
-    JoinedPlayers = []
-
-    global WinAmount
-    WinAmount = 0
-
-    global StartTime
-    StartTime = None
-    global StartData
-    StartData = None
+    global ScriptSettings
+    ScriptSettings = Settings(settingsfile)
 
 def Execute(data):
     """Required Execute data function"""
-    global State
-    global WinAmount
-    global JoinedPlayers
-    global StartTime
-    global StartData
-
     if State == 0 and data.IsChatMessage() and data.GetParam(0).lower() == MySet.Command.lower():
 
         if not HasPermission(data):
@@ -209,14 +146,6 @@ def PickWinner(data):
 
 def Tick():
     """Required tick function"""
-    global StartTime
-    global StartData
-
-    if StartTime is not None:
-        elapsedTime = time.time() - StartTime
-        if elapsedTime > MySet.RaffleTime:
-            PickWinner(StartData)
-
     return
 
 def HasPermission(data):
